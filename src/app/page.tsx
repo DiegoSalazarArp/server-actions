469946f817bdf600f86ab3844dbbe0a8cd9a6e1d
DiegoSalazar <d.salazararp@gmail.com>
2024-10-07 22:42:46 -0300

    Refactorizar código para descomentar el formulario en la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Form from "./components/Form";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-2">
-      {/* <Form /> */}Í
+      <Form />
     </div>
   );
 }
