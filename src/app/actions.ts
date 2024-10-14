edc642214ac92e65b989f194802f53182eb4fc44
DiegoSalazar <d.salazararp@gmail.com>
2024-10-14 12:18:43 -0300

    Refactorizar código para agregar la dependencia de zod

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,24 +1,42 @@
 'use server'
 
-export default async function SubmitAction(prevState: any,
-  formData: FormData) {
+import { z } from 'zod';
+import fs from 'fs/promises'
 
-  // set timeout to simulate a slow network request
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+const formDataSchema = z.object({
+  name: z.string().nonempty('Name is required')
+});
 
+export async function SubmitAction(prevState: any, formData: FormData) {
   try {
-    if (!formData.get('name')) {
-      throw new Error('Name is required');
-    }
+    const parsedData = formDataSchema.parse({
+      name: formData.get('name')
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return {
-      message: `Hello, ${formData.get('name')}`
+      message: `Hello, ${parsedData.name}`
     }
 
-  }
-  catch (e: any) {
+  } catch (e: any) {
     return {
-      error: e.message
+      error: e.errors ? e.errors[0].message : e.message
     }
   }
 }
+
+const secretPath = "public/hello.txt"
+
+export async function CreateFileDirectory() {
+  try {
+    const data = 'Hello, World!';
+    const done = await fs.writeFile(secretPath, data);
+
+    return done
+
+  } catch (e: any) {
+
+  }
+}
+
--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useActionState } from "react";
-import SubmitAction from "../actions";
+import { SubmitAction } from "../actions";
 
 export default function Form() {
   const [state, formAction, pending] = useActionState(SubmitAction, null);
@@ -12,7 +12,7 @@ export default function Form() {
         <h1 className="text-2xl">Enter</h1>
       </div>
       <form action={formAction} className="flex flex-col gap-2">
-        <input placeholder="enter your name" type="text" name="name" className="border border-slate-400 py-2 px-4 rounded" />
+        <input autoComplete="off" placeholder="enter your name" type="text" name="name" className="border border-slate-400 py-2 px-4 rounded" />
         <button type="submit" className="py-2 px-4 bg-blue-400 rounded text-white hover:opacity-80 transition-all duration-100">
           {
             pending ? 'Submitting...' : 'Submit'
